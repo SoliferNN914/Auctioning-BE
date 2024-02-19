b340a5b2e2cc1e3b12c382df891da38fa62f3785
SoliferNN914 <oskarson16@gmail.com>
2024-02-19 13:50:09 +0000

    created all necessary tables, sorted all errors

--- db/data/test-data/events.js
+++ db/data/test-data/events.js
@@ -1,5 +1,5 @@
 const d = new Date()
-const auctionEnd = d.setMinutes(d.getMinutes() + 20)
+// const auctionEnd = d.setMinutes(d.getMinutes() + 20)
 const filmStart1 = d.setHours(d.getHours() + 6)
 const filmStart2 = d.setHours(d.getHours() + 4)
 const filmStart3 = d.setHours(d.getHours() + 8)
@@ -12,23 +12,10 @@ module.exports = [
     certificate: '12',
     run_time: '104',
     start_time: `${filmStart1}`,
-    available_seats: [
-      'A1',
-      'A2',
-      'A3',
-      'A4',
-      'B1',
-      'B2',
-      'B3',
-      'B4',
-      'C1',
-      'C2',
-      'C3',
-      'C4',
-    ],
+    available_seats: '{A1,A2,A3,A4,B1,B2,B3,B4,C1,C2,C3,C4}',
     active: true,
     start_price: 3,
-    business_ID: 1,
+    business_id: 1,
   },
   {
     film_title: 'Madame Webb',
@@ -37,10 +24,10 @@ module.exports = [
     certificate: '12',
     run_time: '117',
     start_time: `${filmStart2}`,
-    available_seats: ['A1', 'A2', 'A3', 'A4', 'B3', 'B4', 'C1', 'C2'],
+    available_seats: '{A1,A2,A3,A4,B3,B4,C1,C2}',
     active: true,
     start_price: 3.5,
-    business_ID: 2,
+    business_id: 2,
   },
   {
     film_title: 'The Holdovers',
@@ -49,26 +36,9 @@ module.exports = [
     certificate: '15',
     run_time: '133',
     start_time: `${filmStart3}`,
-    available_seats: [
-      'A2',
-      'A5',
-      'B1',
-      'B2',
-      'B3',
-      'B4',
-      'B5',
-      'C1',
-      'C2',
-      'D3',
-      'D4',
-      'D5',
-      'E1',
-      'E2',
-      'E4',
-      'E5',
-    ],
+    available_seats: '{A2,A5,B1,B2,B3,B4,B5,C1,C2,D3,D4,D5.E1,E2,E4,E5}',
     active: true,
     start_price: 4,
-    business_ID: 3,
+    business_id: 3,
   },
 ]
