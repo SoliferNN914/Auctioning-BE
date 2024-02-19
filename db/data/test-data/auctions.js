aa655bf755aafc24288534c5495c5061648f7b82
SoliferNN914 <oskarson16@gmail.com>
2024-02-19 11:07:57 +0000

    changed auctions.js

--- db/data/test-data/auctions.js
+++ db/data/test-data/auctions.js
@@ -1,10 +1,23 @@
+const d = new Date()
+const auctionStart1 = d.setMinutes(d.getMinutes())
+const auctionStart2 = d.setMinutes(d.getMinutes() - 40)
+const auctionStart3 = d.setMinutes(d.getMinutes() + 40)
+
+const auctionEnd1 = d.setMinutes(d.getMinutes() + 20)
+const auctionEnd2 = d.setMinutes(d.getMinutes() - 20)
+const auctionEnd3 = d.setMinutes(d.getMinutes() + 60)
+
+//auction1 active now
+//auction2 ended
+//auction3 active in future
+
 module.exports = [
   {
     event_id: 1,
     seat_selection: ['A1', 'A2'],
     current_bid: 5,
-    time_started: '',
-    time_ending: '',
+    time_started: `${auctionStart1}`,
+    time_ending: `${auctionEnd1}`,
     current_highest_bidder: 2,
     users_involved: [1, 2],
     active: false,
@@ -14,8 +27,8 @@ module.exports = [
     event_id: 2,
     seat_selection: ['B3'],
     current_bid: 3.5,
-    time_started: '',
-    time_ending: '',
+    time_started: `${auctionStart2}`,
+    time_ending: `${auctionEnd2}`,
     current_highest_bidder: 3,
     users_involved: [3],
     active: false,
@@ -25,8 +38,8 @@ module.exports = [
     event_id: 3,
     seat_selection: ['D3', 'D4', 'D5'],
     current_bid: 4,
-    time_started: '',
-    time_ending: '',
+    time_started: `${auctionStart3}`,
+    time_ending: `${auctionEnd3}`,
     current_highest_bidder: 4,
     users_involved: [4],
     active: false,
