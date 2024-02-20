64c20229db3a840195aa972c2842173c624de5f8
lauriehillier <laurie.hillier@gmail.com>
2024-02-20 10:22:41 +0000

    Resolved

--- models/auctions.models.js
+++ models/auctions.models.js
@@ -28,15 +28,6 @@ exports.updateAuctionsById = (req) => {
   })
 };
 
-  return db
-    .query(`SELECT * FROM auctions WHERE auctions.event_id = $1`, [event_id])
-    .then((auction) => {
-      if (!auction.rows.length) {
-        return Promise.reject({ status: 404, msg: 'Auction does not exist' })
-      }
-      return auction.rows[0]
-    })
-}
 
 exports.selectAuctionsByUserInvolved = (user_id, active) => {
   if (active && !['true', 'false'].includes(active.toLowerCase())) {
