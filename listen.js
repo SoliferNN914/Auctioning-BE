d6b481cb48b54f626851b0aa90c00344decdba09
lauriehillier <laurie.hillier@gmail.com>
2024-02-22 17:25:42 +0000

    cors allowed

--- listen.js
+++ listen.js
@@ -3,12 +3,16 @@ const app = require('./app.js')
 const { PORT = 9090 } = process.env
 
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+})
 io.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`)
   socket.on('chat message', (msg) => {
-    io.emit('chat message', `Bid Now: ${msg}!`);
-  });
+    io.emit('chat message', `Bid Now: ${msg}!`)
+  })
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
