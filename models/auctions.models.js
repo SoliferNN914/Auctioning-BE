233a8c1cb069e83664bc2c735f3ea2be194f036d
lauriehillier <laurie.hillier@gmail.com>
2024-02-28 09:07:35 +0000

    change auction timer to 5 minutes

--- models/auctions.models.js
+++ models/auctions.models.js
@@ -105,7 +105,7 @@ exports.selectAuctionsWonByUserId = (user_id) => {
 
 exports.insertAuction = (new_auction) => {
   const { event_id, seat_selection, current_price, user_id } = new_auction
-  const auctionEnd = new Date().setSeconds(new Date().getSeconds() + 10)
+  const auctionEnd = new Date().setMinutes(new Date().getMinutes() + 5)
   return checkExists('users', 'user_id', user_id, 'User')
     .then(() => {
       return checkExists('events', 'event_id', event_id, 'Event')
